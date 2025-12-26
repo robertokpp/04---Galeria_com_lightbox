f435cdab3c3fb86e1d331c1063fec54db70de974
Roberto Kennedy <roberto.kpp@gmail.com>
2025-12-26 17:06:28 -0500

    Adicionar atributos de imagem e corrigir funcionalidade do lightbox

--- scripts.js
+++ scripts.js
@@ -1,12 +1,18 @@
 const galleryItem = document.querySelectorAll(".gallery-item");
 const lightbox = document.querySelector(".lightbox");
-const lightContent = document.querySelector(".lightbox-content");
+const lightboxImg = document.querySelector(".lightbox-img");
+const lightClose = document.querySelector(".lightbox-close");
+
 
 galleryItem.forEach((item) => {
   item.addEventListener("click", () => {
-    const imgSrc = item.querySelector(".gallery-item").getAttribute("img")
-    console.log(imgSrc)
-    // lightContent.setAttribute(imgSrc)
-    // console.log(lightContent)
+    const imgSrc = item.querySelector(".gallery-img").getAttribute("data-src");
+    lightboxImg.setAttribute("src", imgSrc);
+    lightbox.style.display = "flex";
+    console.log("teste")
+  });
 });
+
+lightClose.addEventListener("click",() =>{
+  lightbox.style.display = "none";
 })
